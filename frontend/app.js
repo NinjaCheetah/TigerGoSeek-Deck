c5de35f45372b9f7449d8b382520293a7395c25a
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-01-17 01:00:47 -0500

    More minor card formatting changes

--- frontend/app.js
+++ frontend/app.js
@@ -25,7 +25,8 @@ async function updateDisplay(hand) {
     for(let i = 0; i < hand.length; i++) {
         let cardDiv = `
         <div class="col-6">
-            <div class="card" id="card${i}div"><h2>${hand[i]["title"]}</h2>
+            <div class="card" id="card${i}div">
+                <h2 style="padding-top: 1rem;">${hand[i]["title"]}</h2>
                 <p>${hand[i]["description"]}</p>
                 <p><b>${hand[i]["cost"]}</b></p>
                 <input type="checkbox" id="card${i}" name="card" value="card${hand[i]["id"]}"/>
