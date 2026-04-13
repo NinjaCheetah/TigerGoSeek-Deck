7a60dd04840da426366dbe15d26686e7c957de81
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2026-04-13 01:00:46 -0400

    Oops API URL needs to be prod in prod

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,8 +4,8 @@
 //
 // Provides the frontend code for interacting with the backend to display players' cards and trigger updates to them.
 
-const API_URL = "http://localhost:8000"
-//const API_URL = "https://api.tigergoseek.ninjacheetah.dev"
+//const API_URL = "http://localhost:8000"
+const API_URL = "https://api.tigergoseek.ninjacheetah.dev"
 
 // Updates the current URL with the set parameters so that they won't get lost if the page reloads.
 // async function updateURL(username) {
